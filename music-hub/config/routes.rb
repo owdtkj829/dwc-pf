56e3b49114c298d324a7057a204d0670bb734686
oowada takuji <jiinyo829@gmail.com>
2020-04-09 10:43:42 +0900

    ルーティング修正

--- music-hub/config/routes.rb
+++ music-hub/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'home#top'
-  get 'home/about'
+  root :to => 'homes#top'
+  get 'homes/about'
 end
