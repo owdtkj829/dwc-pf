23d2656a74bade7f01a735a5536d34db08258f5a
oowada takuji <jiinyo829@gmail.com>
2020-04-11 19:24:38 +0900

    スケジュールシェアステータス定義

--- music-hub/app/models/schedule.rb
+++ music-hub/app/models/schedule.rb
@@ -3,4 +3,14 @@ class Schedule < ApplicationRecord
 	belongs_to :user
 	belongs_to :artist
 
+	#スケジュールをシェアするかしないか
+	def if_share
+		yuukou = "シェアする"
+		mukou = "シェアしない"
+		if true == share
+			yuukou
+		else
+			mukou
+		end
+	end
 end
