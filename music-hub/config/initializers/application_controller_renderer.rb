63262b187e5ad954958e981e7c55d84c44c0091b
oowada takuji <jiinyo829@gmail.com>
2020-04-11 19:25:14 +0900

    refileシークレットキー記載

--- music-hub/config/initializers/application_controller_renderer.rb
+++ music-hub/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2e683615de95dd24282fe1317eca793c78982b5e0277359da64ec68ee08468ad9ccc8ab253b4d226d82b0dfcabc45bc07064eca8f389b6d8a7dd4e85e7413c7c'
