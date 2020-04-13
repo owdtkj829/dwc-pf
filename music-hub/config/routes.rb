fc8cedd17b8b803686cc9c4b2ab2ba3882c6aa63
oowada takuji <jiinyo829@gmail.com>
2020-04-13 20:22:59 +0900

    ルーティング修正

--- music-hub/config/routes.rb
+++ music-hub/config/routes.rb
@@ -7,11 +7,18 @@ Rails.application.routes.draw do
   #artistお気に入り登録・解除
   post '/artists/:artist_id/favorites' => "favorites#create"
   delete '/artists/:artist_id/favorites' => "favorites#destroy"
+  #recommendお気に入り登録・解除
+  post '/artists/:artist_id/recommends/:recommend_id/myrecommends' => "myrecommends#create"
+  delete '/artists/:artist_id/recommends/:recommend_id/myrecommends' => "myrecommends#destroy"
+  #マイカレンダーJquery用ルーティング
   get '/users/js/jquery.min.js' => "users#show"
   get '/users/js/script.js' => "users#show"
 
-  resources :users, only: [:show, :create, :update, :destroy]
-  resources :artists
+  resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :artists do
+    resources :recommends,only: [:index, :show, :create, :dastroy] do
+      resources :comments,only: [:create, :destroy]
+    end
+  end
   resources :schedules, only: [:index, :show, :create, :update, :destroy]
-
 end
