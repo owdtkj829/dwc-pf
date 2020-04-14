c6d5164a0b89c5433bc8fbdf1f38aa85d1c3c498
oowada takuji <jiinyo829@gmail.com>
2020-04-14 20:17:29 +0900

    ユーザーフォロー機能実装

---
+++ music-hub/app/controllers/relationships_controller.rb
@@ -0,0 +1,36 @@
+class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create,:destroy]
+
+  def show
+    @users = User.all
+    user = User.find(params[:id])
+    
+  end
+
+  def index
+    @users = User.all
+    @user = User.find(params[:id])
+    
+  end
+
+  def create
+    user = User.find(params[:follow_id])
+    following = current_user.follow(user)
+    following.save
+    redirect_back(fallback_location: root_path)
+    
+  end
+
+  def destroy
+    user = User.find(params[:follow_id])
+    following = current_user.unfollow(user)
+    following.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:follow_id])
+  end
+end
