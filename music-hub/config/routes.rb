96d2380d84c13b05aecc2c1dc36b7ad1d90a6613
oowada takuji <jiinyo829@gmail.com>
2020-04-15 22:38:33 +0900

    ルーティング修正

--- music-hub/config/routes.rb
+++ music-hub/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#tops'
   get '/about' => 'home#about'
+  get 'events', to: 'events#events'
+  get 'events_artist', to: 'events#event'
 
   #artistお気に入り登録・解除
   post '/artists/:artist_id/favorites' => "favorites#create"
@@ -14,14 +16,18 @@ Rails.application.routes.draw do
   delete '/artists/:artist_id/recommends/:recommend_id/myrecommends' => "myrecommends#destroy"
   get '/users/:id/myrecommends' => "users#myrecommend",as: 'myrecommends'
 
+  #userのevent一覧表示と詳細表示
+  get '/users/:user_id/events' => "events#user_index",as: 'user_events'
+  get '/users/:user_id/events/:id' => "events#user_show",as: 'user_event'
 
   resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
+
   resources :artists do
-    resources :schedules,only: [:index, :show]
+    resources :events,only: [:index, :show]
     resources :recommends,only: [:index, :show, :create, :dastroy] do
       resources :comments,only: [:create, :destroy]
     end
   end
-  resources :schedules, only: [:index, :show, :create, :update, :destroy]
+  resources :events, only: [:create, :destroy, :update]
   resources :relationships, only: [:create, :destroy]
 end
