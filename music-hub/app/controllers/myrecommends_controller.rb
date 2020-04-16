e5a4ce9bffd8035722b10f0e966068de3737854d
oowada takuji <jiinyo829@gmail.com>
2020-04-16 19:59:20 +0900

    レコメンドのいいね機能、非同期機能実装

--- music-hub/app/controllers/myrecommends_controller.rb
+++ music-hub/app/controllers/myrecommends_controller.rb
@@ -2,16 +2,19 @@ class MyrecommendsController < ApplicationController
 	before_action :set_recommend
 
   def create
-    @myrecommend = Myrecommend.create(user_id: current_user.id, recommend_id: @recommend.id)
+    @myrecommend = Myrecommend.create(user_id: current_user.id, recommend_id: @recommend.id, artist_id: @artist.id)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @myrecommend = Myrecommend.find_by(user_id: current_user.id, recommend_id: @recommend.id)
+    @myrecommend = Myrecommend.find_by(user_id: current_user.id, recommend_id: @recommend.id, artist_id: @artist.id)
     @myrecommend.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
   def set_recommend
+    @artist = Artist.find(params[:artist_id])
     @recommend = Recommend.find(params[:recommend_id])
   end
 end
