5168140bb683dd31e72bf05275b2e928bd0ca3de
oowada takuji <jiinyo829@gmail.com>
2020-04-22 16:05:24 +0900

    Schedulesテーブル削除

--- music-hub/db/schema.rb
+++ music-hub/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_021649) do
+ActiveRecord::Schema.define(version: 2020_04_22_065134) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -101,20 +101,6 @@ ActiveRecord::Schema.define(version: 2020_04_22_021649) do
     t.index ["follow_id"], name: "index_relationships_on_follow_id"
     t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
     t.index ["user_id"], name: "index_relationships_on_user_id"
-  end
-
-  create_table "schedules", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "artist_id"
-    t.text "venue"
-    t.datetime "start"
-    t.datetime "finish"
-    t.text "url"
-    t.text "memo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "share"
-    t.integer "favorite_id"
   end
 
   create_table "users", force: :cascade do |t|
