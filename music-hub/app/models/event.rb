86669f9ad6a062ce05cdf1a943562769852f4fc5
oowada takuji <jiinyo829@gmail.com>
2020-04-22 21:52:29 +0900

    バリデーション修正

--- music-hub/app/models/event.rb
+++ music-hub/app/models/event.rb
@@ -6,17 +6,17 @@ class Event < ApplicationRecord
 	validates :title, {presence: true,length: {maximum: 20}}
 	validates :start, presence: true
 	validates :end, presence: true
+	validate :start_end_check
 	validates :share, presence: true
 	validates :venue, {presence: true,length: {maximum: 30}}
 	validates :url, {presence: true,format: /\A#{URI::regexp(%w(http https))}\z/}
 	validates :memo, {presence: true, length: {maximum: 150}}
-	validate :start_end_check
-	
+
 	#スケジュールをシェアするかしないか
 	enum share: { シェアする:0, シェアしない:1 }
 
   	def start_end_check
-    	errors.add(:end, "を正しく記入してください。") unless
+    	errors.add(:end, "の日付を正しく記入して下さい。") unless
     	self.start < self.end
   	end
 end
