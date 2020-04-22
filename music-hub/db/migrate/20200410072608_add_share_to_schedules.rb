902371ab58d089492d823ab4f108dd9096c0e88e
oowada takuji <jiinyo829@gmail.com>
2020-04-22 16:19:29 +0900

    migrationファイル削除

--- music-hub/db/migrate/20200410072608_add_share_to_schedules.rb
+++
@@ -1,5 +0,0 @@
-class AddShareToSchedules < ActiveRecord::Migration[5.2]
-  def change
-    add_column :schedules, :share, :integer
-  end
-end
--- music-hub/db/migrate/20200411060316_add_favorite_id_to_schedules.rb
+++
@@ -1,5 +0,0 @@
-class AddFavoriteIdToSchedules < ActiveRecord::Migration[5.2]
-  def change
-    add_column :schedules, :favorite_id, :integer
-  end
-end
